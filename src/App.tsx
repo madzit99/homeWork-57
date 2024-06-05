a68443283f4c669f05e6e0b2fb4e7f23e8af2e48
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-05 16:50:32 +0600

    Создал стейт и функции формы

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 import ToolBar from "./Components/ToolBar/ToolBar";
 import UserForm from "./Components/UserForm/UserForm";
 import Users from "./Components/Users/Users";
+import { User } from "./types";
 
 const App = () => {
+  const [users, setUsers] = useState<User[]>([
+    {
+      id: "1",
+      name: "Маджит",
+      email: "madzitaliev@gmail.com",
+      active: true,
+      role: "administrator",
+    },
+    {
+      id: "2",
+      name: "Амир",
+      email: "amirrrr@gmail.com",
+      active: true,
+      role: "editor",
+    },
+  ]);
+
+  const addUser = (user: User) => {
+    setUsers((prev) => [...prev, user]);
+  };
+
   return (
     <>
       <header>
@@ -12,10 +35,10 @@ const App = () => {
         <div className="container">
           <div className="row">
             <div className="col-6">
-              <UserForm/>
+              <UserForm onSubmit={addUser}/>
             </div>
             <div className="col-6">
-              <Users/>
+              <Users users={users}/>
             </div>
           </div>
         </div>
--- src/Components/UserForm/UserForm.tsx
+++ src/Components/UserForm/UserForm.tsx
@@ -1,11 +1,61 @@
-const UserForm = () => {
+import { useState } from "react";
+import { User } from "../../types";
+
+interface Props {
+  onSubmit: (user: User) => void;
+}
+
+const initialState = {
+  name: "",
+  email: "",
+  active: false,
+  role: "",
+};
+
+const UserForm: React.FC<Props> = ({ onSubmit }) => {
+  const [user, setUser] = useState(initialState);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked;
+    setUser((prev) => ({
+      ...prev,
+      active: isChecked,
+    }));
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit({
+      id: Math.random().toString(),
+      ...user,
+    });
+    setUser(initialState);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={onFormSubmit}>
         <h4>Добавить нового пользователя</h4>
         <div className="form-group">
           <label htmlFor="name">Имя</label>
-          <input type="text" name="name" id="name" className="form-control" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            onChange={handleChange}
+            value={user.name}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="email">Почта</label>
@@ -14,6 +64,8 @@ const UserForm = () => {
             name="email"
             id="email"
             className="form-control"
+            onChange={handleChange}
+            value={user.email}
           />
         </div>
         <div className="form-group">
@@ -22,6 +74,8 @@ const UserForm = () => {
             type="checkbox"
             name="checkbox"
             id="checkbox"
+            onChange={handleCheckboxChange}
+            checked={user.active}
           />
           <label htmlFor="checkbox" className="form-check-label ms-2">
             Активен:
@@ -29,7 +83,13 @@ const UserForm = () => {
         </div>
         <div className="form-group">
           <label htmlFor="role">Роль:</label>
-          <select name="role" id="role" className="form-select">
+          <select
+            name="role"
+            id="role"
+            className="form-select"
+            onChange={handleChange}
+            value={user.role}
+          >
             <option defaultValue={"role"}>Выберите роль</option>
             <option value="user">Пользователь</option>
             <option value="editor">Редактор</option>
